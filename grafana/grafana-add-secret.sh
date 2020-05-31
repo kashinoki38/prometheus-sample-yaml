9f2608be539b22d5236db79e15710101c4522bac
kashinoki38 <kashinoki38@gmail.com>
2020-06-01 01:28:57 +0900

    first commit

---
+++ grafana/grafana-add-secret.sh
@@ -0,0 +1,16 @@
+GRAFANA_USERNAME=$(echo -n "grafana" | base64)
+GRAFANA_PASSPHRASE=$(echo -n "poc-telefonica" | base64)
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: grafana
+  namespace: istio-system
+  labels:
+    app: grafana
+type: Opaque
+data:
+  username: $GRAFANA_USERNAME
+  passphrase: $GRAFANA_PASSPHRASE
+EOF
