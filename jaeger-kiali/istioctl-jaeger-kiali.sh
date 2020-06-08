ad255a9a33bdc46718271c439fd90c062a846469
kashinoki38 <kashinoki38@gmail.com>
2020-06-08 13:25:41 +0900

    add jaeger and kiali by istioctl

---
+++ jaeger-kiali/istioctl-jaeger-kiali.sh
@@ -0,0 +1,6 @@
+$ KIALI_USERNAME=$(read -p 'Kiali Username: ' uval && echo -n $uval | base64)
+Kiali Username: admin
+$ KIALI_PASSPHRASE=$(read -sp 'Kiali Passphrase: ' pval && echo -n $pval | base64)
+Kiali Passphrase:
+$ cat <<EOF | kubectl apply -f - apiVersion: v1 kind: Secret metadata: name: kiali namespace: $NAMESPACE labels: app: kiali type: Opaque data: username: $KIALI_USERNAME passphrase: $KIALI_PASSPHRASE EOF
+$ istioctl manifest apply --set values.kiali.enabled=true --set values.tracing.enabled=true
