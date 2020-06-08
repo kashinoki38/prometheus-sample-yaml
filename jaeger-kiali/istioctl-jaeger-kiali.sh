346bafd31a3d671e412afd0c6cfd28cbbeaaaa7b
kashinoki38 <kashinoki38@gmail.com>
2020-06-08 13:26:10 +0900

    modify kiali

--- jaeger-kiali/istioctl-jaeger-kiali.sh
+++ jaeger-kiali/istioctl-jaeger-kiali.sh
@@ -1,6 +1,6 @@
 $ KIALI_USERNAME=$(read -p 'Kiali Username: ' uval && echo -n $uval | base64)
-Kiali Username: admin
+# Kiali Username: admin
 $ KIALI_PASSPHRASE=$(read -sp 'Kiali Passphrase: ' pval && echo -n $pval | base64)
-Kiali Passphrase:
+# Kiali Passphrase:
 $ cat <<EOF | kubectl apply -f - apiVersion: v1 kind: Secret metadata: name: kiali namespace: $NAMESPACE labels: app: kiali type: Opaque data: username: $KIALI_USERNAME passphrase: $KIALI_PASSPHRASE EOF
 $ istioctl manifest apply --set values.kiali.enabled=true --set values.tracing.enabled=true
